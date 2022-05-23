21545bf84d7585f6428147941b1c4f16b682f9ea
mcnxo2001 <minhvietnam147@gmail.com>
2022-05-23 12:07:19 +0700

    tick

--- Pharmacy Management/Schedule.cs
+++ Pharmacy Management/Schedule.cs
@@ -191,7 +191,7 @@ namespace Pharmacy_Management
                         };
                         FirebaseResponse response3 = await Client.UpdateTaskAsync("Schedule/Item" + IdFocus.ToString(), edit);
                         Data result = response3.ResultAs<Data>();
-                        //MessageBox.Show("Đã chỉnh sửa lời nhắc !!!");
+                        //MessageBox.Show("Đã chỉnh sửa lời nhắc !!!");1
                     }
                 }
             }
